06ab454739d7731db1b0b4d682c1585c6e122e8b
cclp94 <cclp94@gmail.com>
2017-12-14 17:02:53 -0500

    Made functions for turning and reverse

--- dogglebot.ino
+++ dogglebot.ino
@@ -2,10 +2,10 @@
 #include <util/delay.h>
 
 /*setting pins for wheels*/
-// Wheel 1 (back right)
+// Wheel 1 (back left)
 int wheel1A = PC1;
 int wheel1B = PC0;
-// Wheel 2 (back left)
+// Wheel 2 (back right)
 int wheel2A = PC5;
 int wheel2B = PC4;
 // Wheel 3 (front right)
@@ -20,15 +20,84 @@ void setupWheels(){
   DDRD |= (1 <<  PD4) | (1 << PD5);
 }
 
-void moveFoward(){
-    PORTC |= (0 << wheel1A) | (1<< wheel1B) | (0 << wheel2A) | (1<< wheel2B) | (0 << wheel4A) | (1<< wheel4B);
+void moveWheel1(bool reverse){
+  if(!reverse)
+    PORTC |= (0 << wheel1A) | (1<< wheel1B);
+  else
+    PORTC |= (1 << wheel1A) | (0<< wheel1B);
+}
+
+void moveWheel2(bool reverse){
+  if(!reverse)
     PORTD |= (1 << wheel3A) | (0 <<wheel3B);
+  else
+    PORTD |= (0 << wheel3A) | (1 << wheel3B);
+}
+
+
+void moveWheel3(bool reverse){
+  if(!reverse)
+    PORTC |= (0 << wheel4A) | (1<< wheel4B);
+  else
+    PORTC |= (1 << wheel4A) | (0<< wheel4B) ;
+}
+
+void moveWheel4(bool reverse){
+  if(!reverse)
+    PORTC |= (0 << wheel2A) | (1<< wheel2B) ;
+  else
+    PORTC |= (1 << wheel2A) | (0<< wheel2B) ;
+}
+
+void stopWheel1(){
+  PORTC |= (0 << wheel1A) | (0<< wheel1B);
+}
+
+void stopWheel2(){
+  PORTD |= (0 << wheel3A) | (0 <<wheel3B);
+}
+
+
+void stopWheel3(){
+  PORTC |= (0 << wheel4A) | (0<< wheel4B);
+}
+
+void stopWheel4(){
+  PORTC |= (0 << wheel2A) | (0<< wheel2B) ;
+}
+
+void moveFoward(){
+    moveWheel1(false);
+    moveWheel2(false);
+    moveWheel3(false);
+    moveWheel4(false);
+}
+
+void turnLeft(){
+    stopWheel1();
+    moveWheel2(false);
+    stopWheel3();
+    moveWheel4(false);
+}
+
+void turnRight(){
+    moveWheel1(false);
+    stopWheel2();
+    moveWheel3(false);
+    stopWheel4();
+}
+
+void reverse(){
+    moveWheel1(true);
+    moveWheel2(true);
+    moveWheel3(true);
+    moveWheel4(true);
 }
 
 int main(){
  setupWheels();
   
   while(1)
-    moveFoward();
+    reverse();
  return 0; 
 }
