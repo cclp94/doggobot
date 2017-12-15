a33196c57c95e6ab8d24f379cbb2d10455a65b84
root <root@beaglebone.localdomain>
2017-12-15 07:17:22 +0000

    Added beagle spi program

--- doggoBrain.py
+++ doggoBrain.py
@@ -1,3 +1,33 @@
 from Adafruit_BBIO.SPI import SPI
- 
-spi = SPI(0,0)	#/dev/spidev1.0
+
+WALK = 1
+WALK_BACK = 31
+STOP = 15
+TURNL = 3
+TURNR = 7
+
+def getCommandCode(command):
+  if command == 'w':
+    print("Walk!")
+    return WALK
+  elif command == 'b':
+    print('Reverse')
+    return WALK_BACK
+  elif command == 's':
+    print('Stop!')
+    return STOP
+  elif command == 'l': 
+    print('Turn Left')
+    return TURNL
+  elif command == 'r':
+    print('Turn Right')
+    return TURNR
+  return 0
+
+spi = SPI(0,0) 
+spi.msh = 57600
+spi.bpw = 8
+while True:
+  command = raw_input();
+  spi.xfer2([getCommandCode(command)])
+spi.close() 
